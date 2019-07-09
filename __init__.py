1cdf9e588d8e2c6421f8a269909df7283a903b7f
Michal Melewski <michal.melewski@gmail.com>
2019-07-09 23:11:37 +0200

    Tiny improvments

--- __init__.py
+++ __init__.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 # author: carstein <michal.melewski@gmail.com>
-# Annotate syscalls with prototype
+# Annotate syscalls with arguments
 
-import os
-import json
 from binaryninja import PluginCommand
 
-from modules import syscaller
+from .modules import syscaller
 
 # register plugin
 PluginCommand.register_for_function(
-  "[sysc] Decorate syscalls",
+  "Syscaller/Decorate syscalls in function",
   "Annotate syscalls with arguments",
   syscaller.run_plugin)
